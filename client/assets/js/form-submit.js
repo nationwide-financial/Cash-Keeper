6ea3845d43091146fe77df239b0429c5c72896eb
williamboomer87 <126490002+williamboomer87@users.noreply.github.com>
2024-11-27 03:53:38 +0530

    Update form-submit.js

--- client/assets/js/form-submit.js
+++ client/assets/js/form-submit.js
@@ -4,26 +4,26 @@ document.querySelector('.offer-form').addEventListener('submit', async function 
     const data = Object.fromEntries(formData.entries());
     console.log(data)
 
-    // try {
-    //     const response = await fetch('https://your-api-endpoint.com/submit', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //         },
-    //         body: JSON.stringify(data),
-    //     });
-
-    //     if (response.ok) {
-    //         const result = await response.json();
-    //         alert('Form submitted successfully!'); 
-    //         console.log('Server Response:', result);
-    //     } else {
-    //         const error = await response.json();
-    //         alert('Failed to submit the form.'); 
-    //         console.error('Error:', error);
-    //     }
-    // } catch (error) {
-    //     console.error('Network Error:', error);
-    //     alert('Network error. Please try again.');
-    // }
+    try {
+        const response = await fetch('https://cashkeeperfinancial.netlify.app/.netlify/functions/env', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: {},
+        });
+        console.log(response?.data)
+        // if (response.ok) {
+        //     const result = await response.json();
+        //     alert('Form submitted successfully!'); 
+        //     console.log('Server Response:', result);
+        // } else {
+        //     const error = await response.json();
+        //     alert('Failed to submit the form.'); 
+        //     console.error('Error:', error);
+        // }
+    } catch (error) {
+        console.error('Network Error:', error);
+        alert('Network error. Please try again.');
+    }
 });
