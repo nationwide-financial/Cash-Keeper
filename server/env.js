ac1ace7a5298668786af4b862091fed4daee57eb
williamboomer87 <126490002+williamboomer87@users.noreply.github.com>
2024-11-27 03:28:02 +0530

    update to add axios request

--- server/env.js
+++ server/env.js
@@ -1,15 +1,30 @@
+const axios = require('axios');
+
 exports.handler = async (event, context) => {
     try {
-        const key = process.env.API_KEY
+        const apiKey = process.env.API_KEY; // Ensure API_KEY is set in your environment variables
+        const apiUrl = 'https://7fwwglseys3xlqk6hogiazspv40gzoug.lambda-url.us-east-1.on.aws/api/generate-token'; // Replace with your API endpoint
+
+        // Make the POST request
+        const response = await axios.post(
+            apiUrl,
+            {}, // If your API expects a request body, add it here
+            {
+                headers: {
+                    'x-api-key': apiKey,
+                },
+            }
+        );
+        const token = response.data.token; 
         return {
             statusCode: 200,
-            body: JSON.stringify({kay : key}),
+            body: JSON.stringify({ token }),
         };
     } catch (error) {
-        console.error('Error connecting to the database:', error);
+        console.error('Error connecting to the API:', error.message);
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'Error retrieving data' }),
+            body: JSON.stringify({ error: 'Error retrieving token' }),
         };
     }
 };
