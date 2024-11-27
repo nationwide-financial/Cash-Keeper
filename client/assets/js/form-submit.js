9cbb18cced606bb3e36a7af072a0d3d8954061bb
williamboomer87 <126490002+williamboomer87@users.noreply.github.com>
2024-11-27 09:54:42 +0530

    Update form-submit.js

--- client/assets/js/form-submit.js
+++ client/assets/js/form-submit.js
@@ -12,29 +12,30 @@ document.querySelector('.offer-form').addEventListener('submit', async function 
             },
             body: {},
         });
-        const token = response?.data?.token
-        if (response.ok) {
-            const response = await fetch('https://7fwwglseys3xlqk6hogiazspv40gzoug.lambda-url.us-east-1.on.aws/api/create-lead-basic-info', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}` // Replace `yourToken` with the actual token value
-                },
-                body: JSON.stringify({ // Convert the body to a JSON string
-                    "offer_code": data?.access_code,
-                    "first_name": data?.first_name,
-                    "last_name": data?.last_name,
-                    "email": data?.email,
-                    "phone_number": data?.phone,
-                    "loan_amount": data?.debt_amount,
-                }),
-            });
-            console.log(response?.data)
-        } else {
-            const error = await response.json();
-            alert('Failed to submit the form.'); 
-            console.error('Error:', error);
-        }
+        const token = response?.data;
+        console.log(token)
+        // if (response.ok) {
+        //     const response = await fetch('https://7fwwglseys3xlqk6hogiazspv40gzoug.lambda-url.us-east-1.on.aws/api/create-lead-basic-info', {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json',
+        //             'Authorization': `Bearer ${token}` // Replace `yourToken` with the actual token value
+        //         },
+        //         body: JSON.stringify({ // Convert the body to a JSON string
+        //             "offer_code": data?.access_code,
+        //             "first_name": data?.first_name,
+        //             "last_name": data?.last_name,
+        //             "email": data?.email,
+        //             "phone_number": data?.phone,
+        //             "loan_amount": data?.debt_amount,
+        //         }),
+        //     });
+        //     console.log(response?.data)
+        // } else {
+        //     const error = await response.json();
+        //     alert('Failed to submit the form.'); 
+        //     console.error('Error:', error);
+        // }
     } catch (error) {
         console.error('Network Error:', error);
         alert('Network error. Please try again.');
