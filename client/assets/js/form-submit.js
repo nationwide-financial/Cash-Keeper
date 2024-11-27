8633b5131774a508f2beb7d61668f19640554506
williamboomer87 <126490002+williamboomer87@users.noreply.github.com>
2024-11-27 10:48:05 +0530

    Update form-submit.js

--- client/assets/js/form-submit.js
+++ client/assets/js/form-submit.js
@@ -5,41 +5,41 @@ document.querySelector('.offer-form').addEventListener('submit', async function 
     console.log(data)
 
     try {
-        const response = await fetch('https://cashkeeperfinancial.netlify.app/.netlify/functions/env', {
+        const res = await fetch('https://cashkeeperfinancial.netlify.app/.netlify/functions/env', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: {},
         });
-        response?.json();
     
-        response.json().then((data) => {
-            console.log(data)
-        })
+      
 
-        // if (response.ok) {
-        //     const response = await fetch('https://7fwwglseys3xlqk6hogiazspv40gzoug.lambda-url.us-east-1.on.aws/api/create-lead-basic-info', {
-        //         method: 'POST',
-        //         headers: {
-        //             'Content-Type': 'application/json',
-        //             'Authorization': `Bearer ${token}` // Replace `yourToken` with the actual token value
-        //         },
-        //         body: JSON.stringify({ // Convert the body to a JSON string
-        //             "offer_code": data?.access_code,
-        //             "first_name": data?.first_name,
-        //             "last_name": data?.last_name,
-        //             "email": data?.email,
-        //             "phone_number": data?.phone,
-        //             "loan_amount": data?.debt_amount,
-        //         }),
-        //     });
-        //     console.log(response?.data)
-        // } else {
-        //     const error = await response.json();
-        //     alert('Failed to submit the form.'); 
-        //     console.error('Error:', error);
-        // }
+        if (response.ok) {
+            const responseData = await res.json(); // Process response once
+            console.log(responseData)
+
+            // const response = await fetch('https://7fwwglseys3xlqk6hogiazspv40gzoug.lambda-url.us-east-1.on.aws/api/create-lead-basic-info', {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //         'Authorization': `Bearer ${token}` // Replace `yourToken` with the actual token value
+            //     },
+            //     body: JSON.stringify({ // Convert the body to a JSON string
+            //         "offer_code": data?.access_code,
+            //         "first_name": data?.first_name,
+            //         "last_name": data?.last_name,
+            //         "email": data?.email,
+            //         "phone_number": data?.phone,
+            //         "loan_amount": data?.debt_amount,
+            //     }),
+            // });
+            // console.log(response?.data)
+        } else {
+            const error = await response.json();
+            alert('Failed to submit the form.'); 
+            console.error('Error:', error);
+        }
     } catch (error) {
         console.error('Network Error:', error);
         alert('Network error. Please try again.');
