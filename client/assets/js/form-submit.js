9074b936f60e0d7b438e44854018ad423da4de8a
williamboomer87 <126490002+williamboomer87@users.noreply.github.com>
2024-11-27 10:21:55 +0530

    Update form-submit.js

--- client/assets/js/form-submit.js
+++ client/assets/js/form-submit.js
@@ -12,8 +12,8 @@ document.querySelector('.offer-form').addEventListener('submit', async function 
             },
             body: {},
         });
-        //const token = response?.data;
-        console.log(response)
+        const res = response?.json();
+        console.log(res)
         // if (response.ok) {
         //     const response = await fetch('https://7fwwglseys3xlqk6hogiazspv40gzoug.lambda-url.us-east-1.on.aws/api/create-lead-basic-info', {
         //         method: 'POST',
